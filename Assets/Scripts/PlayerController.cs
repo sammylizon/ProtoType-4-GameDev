bdf04acfeb686481fd592554fd935c291b666ffd
Sammy Lizon <lizon_sammy@yahoo.co.uk>
2024-10-20 10:53:00 +0100

    Camera and movement input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Rigidbody rb;
+    public float moveSpeed;
+
+    public GameObject focalPoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetAxisRaw("Vertical") > 0.1){
+            rb.AddForce(moveSpeed * Time.deltaTime * focalPoint.transform.forward);
+        }
         
     }
 }
