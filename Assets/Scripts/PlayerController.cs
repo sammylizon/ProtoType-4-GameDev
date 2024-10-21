1acb4793eac7c3d6001a7d2e922fcd5e5a02927d
Sammy Lizon <lizon_sammy@yahoo.co.uk>
2024-10-21 23:15:34 +0100

    Adding power up indicator and notes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,8 @@ public class PlayerController : MonoBehaviour
     public GameObject focalPoint;
 
     public bool hasPowerUp = false;
+
+    public GameObject powerUpIndicator;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,35 +25,45 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         float input = Input.GetAxis("Vertical");
-            rb.AddForce(moveSpeed* input * focalPoint.transform.forward);
+        //Get vertical input and add force in forward direction
+        float input = Input.GetAxis("Vertical");
+        rb.AddForce(moveSpeed* input * focalPoint.transform.forward);
+
+        //place the indicator at player position with offset
+        powerUpIndicator.transform.position = transform.position - new Vector3(0,0.5f,0);
         
         
     }
 
     private void OnTriggerEnter(Collider other){
+
+        //if player collides with power up the apply power
         if(other.CompareTag("PowerUp")){
             hasPowerUp = true;
             Debug.Log("POWER UP!!! GO GO");
             Destroy(other.gameObject);
             StartCoroutine(PowerUpCountDownRoutine());
+            powerUpIndicator.SetActive(true);
         }
     }
 
+    //Interface used to set up Timer with Coroutine(outside of Update or start)
     IEnumerator PowerUpCountDownRoutine(){
         yield return new WaitForSeconds(7);
         hasPowerUp = false; 
+        powerUpIndicator.SetActive(false);
     }
 
     public void OnCollisionEnter(Collision other){
-        if(other.gameObject.CompareTag("Enemy")&& hasPowerUp){
-            Debug.Log($"You collided with {other.gameObject.name} and Power up is : {hasPowerUp}. Kill on!");
-            Debug.Log("I am {king}! You got destroyed bitch!");
+        //if player has power up and collides with enemy then use buffs
+        if(other.gameObject.CompareTag("Enemy")&& hasPowerUp)
+        {
+            //Get enemy reference and direction to send them away from player. 
             Rigidbody enemy = other.gameObject.GetComponent<Rigidbody>();
             Vector3 awayFromPlayer = (other.gameObject.transform.position - transform.position);
 
+            //Buff sends enemy flying away from player
             enemy.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
-            // Destroy(other.gameObject);
         }
     }
 
